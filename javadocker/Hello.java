9e9ba8e3461beb005c7d32f6a205b9bbd6eea32d
unknown <gadalai426@gmail.com>
2024-01-04 13:55:10 +0800

    dalai

---
+++ javadocker/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+  public static void main(String[] args) {
+    if(args!=null && args.length>0){
+      System.out.println("Hello , "+args[0]+"!");
+    }else{
+      System.out.println("Hello world");
+    }
+  }
+}
